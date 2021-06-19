fc2d66b1fe7d4f58788507d612929f428a4f8ef5
Withr-code <bmuzyka9@gmail.com>
2021-06-19 17:37:06 +0300

    init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private Transform _thiefPosition;
+    [SerializeField] private UnityEvent _opened;
+
+    private void Update()
+    {
+        if (transform.position.x < _thiefPosition.position.x)
+            _opened?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private float _currentStrength;
+    [SerializeField] private float _maxStrength;
+    [SerializeField] private float _maxDelta;
+
+    private void Update()
+    {
+        transform.Translate(Mathf.MoveTowards(_currentStrength, _maxStrength, _maxDelta) * Time.deltaTime, 0, 0);
+    }
+
+    private void OnValidate()
+    {
+        if (_currentStrength > _maxStrength)
+            _maxStrength = _currentStrength;
+    }
+
+    public void ChangeDirection()
+    {
+        _currentStrength *= -1;
+        _maxStrength *= -1;
+    }
+}
