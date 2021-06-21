5554a42b27ee69b9bf8bcaf28c33a73eaabc7ceb
Withr-code <bmuzyka9@gmail.com>
2021-06-21 21:06:48 +0300

    changes to alarm.cs

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -5,12 +5,40 @@ using UnityEngine.Events;
 
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private Transform _thiefPosition;
+    [SerializeField] private Transform _thiefTransform;
+    [SerializeField] private Transform _endPosition;
     [SerializeField] private UnityEvent _opened;
+    [SerializeField] private float _timeToFade;
+
+    private AudioSource _audioSource;
+    private float _endPoint;
+    private float _target;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _endPoint = _endPosition.position.x;
+        _target = transform.position.x;
+    }
 
     private void Update()
     {
-        if (transform.position.x < _thiefPosition.position.x)
+        if (transform.position.x < _thiefTransform.position.x)
+        {
             _opened?.Invoke();
+        }
+
+        Fade();
+    }
+
+    private void Fade()
+    {
+        if (_thiefTransform.position.x < _target)
+            _audioSource.volume = (_thiefTransform.position.x - _endPoint) / (_target - _endPoint);
+        else
+            _audioSource.volume = (_target * 2 - _endPoint  - _thiefTransform.position.x) / (_target - _endPoint);
+
+        if (_audioSource.volume < 0)
+            _audioSource.volume = 0;
     }
 }
